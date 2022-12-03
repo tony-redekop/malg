b89b67e33dabd1982a154f23777ddcf44c6c7ace
Tony Redekop <tony.redekop@outlook.com>
2022-12-03 08:16:06 -0800

    feature: create transpose() method to transpose square matrices in place

--- include/matrix2d.hpp
+++ include/matrix2d.hpp
@@ -39,8 +39,17 @@ class Matrix2D
     unsigned get_rows() const { return nrows_; }
     unsigned get_cols() const { return ncols_; }
 
-    // operations    
-    void transpose();
+    // transposes a square matrix in-place    
+    void transpose() {
+      for(unsigned n=0; n <= (this->nrows_ - 2); n++) {
+        for(unsigned m=n+1; m <= (this->nrows_ - 1); m++) {
+          auto temp = this->ptr_[n][m];
+          this->ptr_[n][m] = this->ptr_[m][n];
+          this->ptr_[m][n] = temp;
+        }
+      }
+    };
+    // allows us to index matrix using clean [i][j] syntax
     const T* operator[](unsigned row);
     // matrix + matrix
     const Matrix2D<T>& operator+(const Matrix2D<T>& right) const;
